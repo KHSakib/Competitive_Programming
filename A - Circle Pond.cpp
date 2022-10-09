ba7c11c0705597b19965d0183e75786caeb290c3
KH Sakib <58090738+KHSakib@users.noreply.github.com>
2022-10-09 16:59:00 +0600

    Create A - Circle Pond.cpp

---
+++ A - Circle Pond.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define pi 3.14159
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    double ans=2*pi*(n*1.00);
+
+    cout<<fixed<<setprecision(20)<<ans<<endl;
+
+    return 0;
+}
